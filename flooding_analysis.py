98b14c9aad28f443c0643402d1899760672de7ed
Giombi <giombilo@LM5-500-36598.local>
2026-03-19 14:14:16 +0100

    Added computation of flooded area

--- flooding_analysis.py
+++ flooding_analysis.py
@@ -29,31 +29,42 @@ for file in os.listdir(DATA_FOLDER_after):                   # get the list of f
     if "decibel_gamma0" in file and file.endswith(".tiff"):       # Band 4 = Red light (~665 nm). Plants absorb red light during photosynthesis.
         after_path = os.path.join(DATA_FOLDER_after, file)
 
-
-# 1. Load BEFORE image
+# 1. Load images
+# BEFORE image
 with rasterio.open(before_path) as src:
     before_db = src.read(1).astype(float)
     profile = src.profile
 
-# 1. Load AFTER image
+# AFTER image
 with rasterio.open(after_path) as src:
     after_db = src.read(1).astype(float)
+#--------------------------------------------------------------------------------------------------
 
-
-# 2. Convert dB → linear scale
+# 2. Compute ratio of backscatter (after/before) in linear scale
+# Convert dB → linear scale
 before = 10 ** (before_db / 10)
 after = 10 ** (after_db / 10)
 
-# 3. Speckle noise reduction
+# 3. Speckle noise reduction (median filter)
 before_filtered = median_filter(before, size=3)
 after_filtered = median_filter(after, size=3)
 
 # 4. Compute ratio
 ratio = after_filtered / (before_filtered + 1e-6)
 
+#--------------------------------------------------------------------------------------------------
 
-# 5. Detect flood (adaptive threshold)
-threshold = 1.   # tune this threshold based on the ratio values (e.g., 0.8, 1.0, 1.2)
+
+# 5. Determine pixels of interest using automatic thresholding (Otsu's method)
+from skimage.filters import threshold_otsu
+
+# Flatten valid values: filter out non-finite values (e.g., due to division by zero or masked areas)
+valid_pixels = ratio[np.isfinite(ratio)]
+
+# Compute threshold automatically using Otsu's method, which finds the threshold that minimizes intra-class variance in the histogram of pixel values
+threshold = threshold_otsu(valid_pixels)
+print("Auto threshold:", threshold)
+
 flood_mask = ratio < threshold  # areas where backscatter decreased significantly (flooded areas) will have ratio < 1
 # print(f"Flood pixels detected: {np.sum(flood_mask)}")
 
@@ -62,6 +73,7 @@ flood_mask = ratio < threshold  # areas where backscatter decreased significantl
 flood_clean = binary_opening(flood_mask, structure=np.ones((3,3))) # The opening of an input image by a structuring element is 
                                                                    # the dilation of the erosion of the image by the structuring element
 
+#--------------------------------------------------------------------------------------------------
 
 
 # 7. Visualization
@@ -76,7 +88,7 @@ axes[2].imshow(flood_clean, cmap="Blues")
 axes[2].set_title("Detected Flood")
 axes[2].axis("off")
 plt.tight_layout()
-plt.show()
+# plt.show()
 
 fig.savefig("flood_detection.png", dpi=300)
 
@@ -87,4 +99,38 @@ profile.update(dtype=rasterio.uint8, count=1)
 with rasterio.open("flood_map.tiff", "w", **profile) as dst:
     dst.write(flood_clean.astype(rasterio.uint8), 1)
 
-print("Flood map saved as flood_map.tiff")
+# print("Flood map saved as flood_map.tiff")
+
+
+fig_over, ax_over = plt.subplots(figsize=(8, 6))
+
+ax_over.imshow(after_db, cmap="gray")
+ax_over.imshow(flood_clean, cmap="Blues", alpha=0.4)
+
+ax_over.set_title("Flood Overlay on SAR Image")
+ax_over.axis("off")
+fig_over.savefig("flood_overlay.png", dpi=300)
+
+#--------------------------------------------------------------------------------------------------
+
+
+# 9. Compute flooded area (km²)
+# Get pixel size (meters)
+pixel_size_x = profile["transform"][0]
+pixel_size_y = -profile["transform"][4]
+pixel_area_m2 = pixel_size_x * pixel_size_y # area of one pixel in square meters (e.g., 10m x 10m = 100 m²)
+
+# Count flooded pixels
+flood_pixels = np.sum(flood_clean)
+
+# Total flooded area
+flood_area_m2 = flood_pixels * pixel_area_m2
+flood_area_km2 = flood_area_m2 / 1e6
+
+print(f"Flooded area: {flood_area_km2:.2f} km²")
+
+
+
+
+
+plt.show()
